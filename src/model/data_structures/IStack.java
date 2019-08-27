d291f1d8062452aa1f58b969d9701dbb7fc10a1d
daniellondonob <ds.londono@uniandes.edu.co>
2019-08-26 19:54:01 -0500

    Implementacion de las Interfaces IStack.java e IQueue.java

--- src/model/data_structures/IStack.java
+++ src/model/data_structures/IStack.java
@@ -1,5 +1,23 @@
 package model.data_structures;
 
-public interface IStack {
+public interface IStack 
+{
+	//Lista de metodos de la clase Stack.java
+
+	boolean isEmpty();
+
+	int size();
+
+	void push(Item item);
+
+	Item pop();
+
+	Item peek();
+
+	String toString();
+
+	Iterator<Item> iterator();
+
+	
 
 }
